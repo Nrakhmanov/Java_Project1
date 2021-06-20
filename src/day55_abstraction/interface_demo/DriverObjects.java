2ffe8d7dedd05d497243ea2dfb4c056d9e243220
Nuriddin Rakhmanov <nuriddin.rakhmanov@gmail.com>
2021-06-20 15:05:44 -0600

    Day 55. Interface examples

---
+++ src/day55_abstraction/interface_demo/DriverObjects.java
@@ -0,0 +1,17 @@
+package day55_abstraction.interface_demo;
+
+public class DriverObjects {
+    public static void main(String[] args) {
+        ChromeDriver driver1 = new ChromeDriver();
+        driver1.get("https://www.google.com");
+        driver1.findElement("//input[@name='q']");
+        System.out.println("Title = " + driver1.getTitle());
+
+        FirefoxDriver driver2 = new FirefoxDriver();
+        driver2.get("https://www.");
+
+        WebDriver driver3 = new ChromeDriver(); // POLYMORPHISM
+
+
+    }
+}
